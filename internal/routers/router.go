b5e582cc5f7d0c2be90eb5997513291a0561a791
daix <daixi859@163.com>
2021-07-09 16:45:02 +0800

    添加文档

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -3,13 +3,18 @@ package routers
 import (
 	v1 "go-service/internal/routers/api/v1"
 
+	_ "go-service/docs"
+
 	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 func NewRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	article := v1.NewArticle()
 	tag := v1.NewTag()
